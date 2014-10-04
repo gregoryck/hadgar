9072600f5416166dd991282fa0f2283643ce8c9d
Greg Kettler <gkettler@alum.mit.edu>
2014-10-04 08:36:49 -0500

    hello lens

--- Main.hs
+++ Main.hs
@@ -1,5 +1,30 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 import Control.Lens
+
+data Thing = Thing
+    { 
+      _val :: String
+    } deriving (Show)
+
+
+makeLenses ''Thing
+
+           
+initial = Thing "initial"
 
 
 main :: IO ()
-main = print "hello lens?"
+main = do
+    print "hello lens?"
+    x <- getLine
+    -- putStrLn x
+    putStrLn $ initial^.val
+    putStrLn $ view val initial
+    let y = set val x initial
+    putStrLn $ view val y
+
+    
+
+    
+
