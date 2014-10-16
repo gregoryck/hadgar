75bc0eb79d7a1edffd10272c498bc1e6c3acb068
Greg Kettler <gkettler@alum.mit.edu>
2014-10-16 08:20:54 -0500

    break out functions look at types

--- HelloPipes.hs
+++ HelloPipes.hs
@@ -1,12 +1,25 @@
 
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE KindSignatures #-}
+
 import Pipes
 import Pipes.HTTP
 import qualified Pipes.ByteString as PB  -- from `pipes-bytestring`
 
+
+-- FIXME better name
+runEffectResp :: forall (m :: * -> *).
+                   MonadIO m =>
+                   Response (Pipes.Proxy X () () PB.ByteString m ()) -> m ()
+runEffectResp resp =
+            runEffect $ responseBody resp >-> PB.stdout
     
+-- FIXME much better name
+withMumble :: Request -> Manager -> IO ()
+withMumble req m =
+        withHTTP req m $ runEffectResp
+
 main :: IO ()
 main = do
     req <- parseUrl "http://www.example.com"
-    withManager tlsManagerSettings $ \m ->
-        withHTTP req m $ \resp ->
-            runEffect $ responseBody resp >-> PB.stdout
+    withManager tlsManagerSettings $ withMumble req
